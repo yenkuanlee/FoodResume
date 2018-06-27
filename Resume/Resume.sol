34d0c151b7abe75f96f136b355e8c7541b2849b8
Kevin Yen-Kuan Lee <yenkuanlee@gmail.com>
2018-06-27 12:13:07 +0800

    add item flow

--- Resume/Resume.sol
+++ Resume/Resume.sol
@@ -10,7 +10,9 @@ contract Resume {
         string[] itemList;
     }
 
-    mapping (address => itemRecord) itemLog;
+    mapping (string => address[]) itemFlow; // some item sales to someone
+    mapping (address => itemRecord) itemLog; // someone generate some items
+    mapping (address => string[]) Notice; // call someone to check
 
     function stringToBytes32(string memory source) returns (bytes32 result) {
         bytes memory tempEmptyStringTest = bytes(source);
@@ -22,6 +24,19 @@ contract Resume {
         }
     }
 
+    function stringsEqual(string storage _a, string memory _b) internal constant returns (bool) {
+        bytes storage a = bytes(_a);
+        bytes memory b = bytes(_b);
+        if (a.length != b.length)
+            return false;
+        for (uint i = 0; i < a.length; i ++)
+            if (a[i] != b[i])
+                return false;
+        return true;
+    }
+
+//==================================================== Start Logic
+
     function Record(string ObjectHash) public returns (string){
         bytes memory tmpOH = bytes(ObjectHash);
         if(tmpOH.length!=46)
@@ -32,34 +47,78 @@ contract Resume {
         return "SUCCESS";
     }
 
-    function GetIndex(address a) public returns(uint256){
-        return itemLog[a].index;
+    function SendNotice(string ObjectHash, address client) public returns(string){
+        bool flag = false;
+        for(uint i=0;i<itemLog[msg.sender].itemList.length;i++){
+            if(stringsEqual(itemLog[msg.sender].itemList[i],ObjectHash))
+                flag = true;
+        }
+        if(flag){
+            Notice[client].length++;
+            Notice[client][Notice[client].length-1] = ObjectHash;
+            return "SUCCESS";
+        }
+        return "NoObjectHashException";
     }
 
-    function GetObjectHash(address a, uint256 indexx) public returns(string){
-        return itemLog[a].itemList[indexx];
+    function GetNotice() public returns(string){
+        string memory Stmp = new string(Notice[msg.sender].length*47);
+        bytes memory Btmp = bytes(Stmp);
+        uint k = 0;
+        for(uint i=0;i<Notice[msg.sender].length;i++){
+            bytes memory Ntmp = bytes(Notice[msg.sender][i]);
+            for(uint j=0;j<Ntmp.length;j++)
+                Btmp[k++] = Ntmp[j];
+            Btmp[k++] = ";";
+        }
+        return string(Btmp);
     }
 
-    function GetStringInfo(address a)public returns(string){
-        uint256 indexx = GetIndex(a);
+    function FillItem(string ObjectHash) public returns(string){
+        bool flag = false;
+        for(uint i=0;i<Notice[msg.sender].length;i++){
+            if(stringsEqual(Notice[msg.sender][i],ObjectHash))
+                flag = true;
+        }
+        if(flag){
+            itemFlow[ObjectHash].length++;
+            itemFlow[ObjectHash][itemFlow[ObjectHash].length-1] = msg.sender;
+            return "SUCCESS";
+        }
+        return "NoObjectHashException";
+    }
+
+    function GetStringItemFlow(string ObjectHash) public returns(address[]){
+        return itemFlow[ObjectHash];
+    }
+
+    function GetIndex() public returns(uint256){
+        return itemLog[msg.sender].index;
+    }
+
+    function GetObjectHash(uint256 indexx) public returns(string){
+        return itemLog[msg.sender].itemList[indexx];
+    }
+
+    function GetStringInfo()public returns(string){
+        uint256 indexx = GetIndex();
         string memory Stmp = new string(indexx*47);
         bytes memory Btmp = bytes(Stmp);
         uint k = 0;
         for(uint256 i=0;i<indexx;i++){
-            bytes memory Ntmp = bytes(GetObjectHash(a,indexx-1-i));
+            bytes memory Ntmp = bytes(GetObjectHash(indexx-1-i));
             for(uint j=0;j<Ntmp.length;j++)
                 Btmp[k++] = Ntmp[j];
-            if(i!=indexx-1)
-                Btmp[k++] = ";";
+            Btmp[k++] = ";";
         }
         return string(Btmp);
     }
-    function GetInfo(address a) public returns(bytes32[]){
-        uint256 indexx = GetIndex(a);
+    function GetInfo() public returns(bytes32[]){
+        uint256 indexx = GetIndex();
         bytes32[] bytesArray;
         bytesArray.length = indexx;
         for(uint256 i=0;i<indexx;i++){
-            bytesArray[i] = stringToBytes32(GetObjectHash(a,indexx-1-i));
+            bytesArray[i] = stringToBytes32(GetObjectHash(indexx-1-i));
         }
         return bytesArray;
     }
--- Resume/test.py
+++ Resume/test.py
@@ -7,12 +7,14 @@ import sys
 Cpath = os.path.dirname(os.path.realpath(__file__))
 
 host = 'localhost'
-account = '0x42946c2bb22ad422e7366d68d3ca07fb1862ff36'
+#account = '0x42946c2bb22ad422e7366d68d3ca07fb1862ff36' ## supplier
+account = '0xe6ab871f860d9f28764d5d2e0672396a7643710e'  ## gmeal
 passwd = '123'
 
 
 # web3.py instance
 w3 = Web3(HTTPProvider('http://'+host+':3000'))
+w3.eth.defaultAccount = account
 w3.personal.unlockAccount(account,passwd)
 f = open(Cpath+'/resume.json','r')
 line = f.readline()
@@ -26,8 +28,26 @@ contract_address = Jline['contract_address']
 # Contract instance in concise mode
 contract_instance = w3.eth.contract(abi, contract_address, ContractFactoryClass=ConciseContract)
 
-#contract_instance.Record(sys.argv[1],transact={'from': account})
-a = contract_instance.GetStringInfo("0x42946c2bb22ad422e7366d68d3ca07fb1862ff36")
-tmp = a.split(";")
-for x in tmp:
-    print(x)
+if sys.argv[1] == "GetStringInfo":
+    a = contract_instance.GetStringInfo()
+    tmp = a.split(";")
+    for x in tmp:
+        print(x)
+elif sys.argv[1] == "SendNotice":
+    a = contract_instance.SendNotice("QmTAW2mzTE1dPdPcHxGhEpVaQLeaUj2nNRm3iHqnBMJhwR","0xe6ab871f860d9f28764d5d2e0672396a7643710e",transact={'from': account})
+    print(a)
+elif sys.argv[1] == "GetNotice":
+    a = contract_instance.GetNotice()
+    print(a)
+elif sys.argv[1] == "GetIndex":
+    a = contract_instance.GetIndex()
+    print(a)
+elif sys.argv[1] == "FillItem":
+    a = contract_instance.FillItem("QmTAW2mzTE1dPdPcHxGhEpVaQLeaUj2nNRm3iHqnBMJhwR")
+    print(a)
+    if a=="SUCCESS":
+        a = contract_instance.FillItem("QmTAW2mzTE1dPdPcHxGhEpVaQLeaUj2nNRm3iHqnBMJhwR",transact={'from': account})
+    print("TransactionID : "+a)
+elif sys.argv[1] == "GetStringItemFlow":
+    a = contract_instance.GetStringItemFlow("QmTAW2mzTE1dPdPcHxGhEpVaQLeaUj2nNRm3iHqnBMJhwR")
+    print(a)
